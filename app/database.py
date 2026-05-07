96e555de1c4c34ce171a82c709da61a69523e224
Dhruvi Gaur <dhruvigaur30@gmail.com>
2026-05-07 18:36:19 +0530

    Fixed Railway PostgreSQL connection

--- app/database.py
+++ app/database.py
@@ -1,41 +1,27 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
-# Load environment variables
-load_dotenv(dotenv_path=".env")
+load_dotenv()
 
-# Get database URL
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Debugging line
-print("DATABASE_URL =", DATABASE_URL)
-
-# Create database engine
 engine = create_engine(DATABASE_URL)
 
-# Create session factory
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# Base model class
 Base = declarative_base()
 
 
-# Database dependency
-# Opens DB session
-# Closes automatically after request
-
 def get_db():
     db = SessionLocal()
-
     try:
         yield db
-
     finally:
         db.close()
